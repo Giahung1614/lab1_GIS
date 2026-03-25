347f4ff2d86dcba1661324c019d5c04cc6088dda
JAJA <danggiahung2004@gmail.com>
2026-03-25 18:34:31 +0700

    add code

---
+++ location_data/dai_hoc.js
@@ -0,0 +1,27 @@
+var truong_template = {
+    title: "{TenTruong}",
+    content: "Năm thành lập: <b>{NamThanhLap}</b><br>Ngành đào tạo: <b>{NganhDaoTao}</b><br>Số lượng sinh viên: <b>{SoSinhVien}</b>"
+};
+
+var dai_hoc_data = {
+    "points": [
+        {
+            type: "point",
+            longitude: 106.80335714985944,
+            latitude: 10.86980521540401,
+            TenTruong: "ĐH Công nghệ Thông tin (UIT)",
+            NamThanhLap: "2006",
+            NganhDaoTao: "KTPM, KHMT, ATTT, HTTT, MMT&TT",
+            SoSinhVien: "Khoảng 8.000",
+            symbol: {
+                type: "picture-marker",
+                url: "./icons/map-pin.png",
+                width: "30px",
+                height: "30px",
+                yoffset: "15px"
+            },
+            popupTemplate: truong_template
+        }
+        // Bạn làm trường ĐH sẽ copy/paste thêm xuống dưới đây
+    ]
+};
